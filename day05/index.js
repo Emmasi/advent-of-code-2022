eadf4f96b90d3d4fca4703bb3eb6e62d33940fc1
Emma <emmaolssondev@outlook.com>
2022-12-06 19:10:32 +0100

    add day6

--- day05/index.js
+++ day05/index.js
@@ -1,10 +1,31 @@
-const movingArray=[
-   {1:[Z][N][D]},
-   {2:[C]},
-   {3:[M][P]}
-]
+const movingArray = [{ 1: ["Z", "N", "D"] }, { 2: ["C"] }, { 3: ["M", "P"] }];
 const fs = require("fs");
-const data = fs.readFileSync("input.txt", "utf8")
+const data = fs.readFileSync("input.txt", "utf8");
 
-const testData = fs.readFileSync("testinput.txt", "utf8").split("\n").map(x=>x.split(",").map(x=>x.split("-")))
-console.log(testData)
+const testData = fs.readFileSync("testinput.txt", "utf8").split("\n");
+
+const moveInstruktion = testData.map((x) => {
+  const newArray = [{ move: x[5], from: x[12], to: x[17] }];
+  return newArray;
+});
+
+const moveFunktion = (instr,item) =>{
+   const firstrow = item[0]
+   console.log(firstrow)
+   const secendrow =item[1]
+   const thirdrow =item[2]
+   console.log(firstrow)
+   instr.forEach(X=>{
+      const moveValue = X[0].move
+      const fromValue = X[0].from
+      console.log(fromValue)
+      const toValue = X[0].to
+      console.log("Flytta från rad",fromValue," ", moveValue,"sty, till rad",toValue)
+      if(fromValue === firstrow){
+         console.log("jupp")
+      }else{
+         console.log("näpp")
+      }
+   })
+}
+moveFunktion(moveInstruktion,movingArray)
---
+++ day06/index.js
@@ -0,0 +1,3 @@
+const fs = require("fs");
+const testData = fs.readFileSync("testinput.txt", "utf8").split("\n");
+const data = fs.readFileSync("testinput.txt", "utf8").split("\n");
