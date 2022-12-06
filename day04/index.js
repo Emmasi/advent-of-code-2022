b8bcde7b89de17a52f1ef7d5bae3f21452d7fbe8
Emma <emmaolssondev@outlook.com>
2022-12-06 19:03:36 +0100

    bug

--- day04/index.js
+++ day04/index.js
@@ -1,39 +1,25 @@
-
 const fs = require("fs");
 const data = fs.readFileSync("input.txt", "utf8").split("\n").map(x=>x.split(",").map(x=>x.split("-")))
 
 const testData = fs.readFileSync("testinput.txt", "utf8").split("\n").map(x=>x.split(",").map(x=>x.split("-")))
 console.log(testData)
+
 const container =[]
-testData.forEach(x=>{
+data.forEach(x=>{
   const firstsections = x[0]
   const secondsections = x[1]
-  const firstFS = parseInt(firstsections[0])
-  const secondFS = parseInt(firstsections[1])
-  const firstSS = parseInt(secondsections[0])
-  const secondSS = parseInt(secondsections[1])
+  const xStart = parseInt(firstsections[0])
+  const xEnd = parseInt(firstsections[1])
+  const yStart = parseInt(secondsections[0])
+  const yEnd = parseInt(secondsections[1])
 
-  if(firstFS>=firstSS&&firstFS<=secondSS){
-    console.log("första är innanför")
-    if(secondFS>=firstSS&&secondFS<= secondSS){
-      console.log("båda är innanför",firstFS,secondFS,",",firstSS,secondSS)
+  if(xStart<=yEnd && xStart>=yStart && xEnd<=yEnd && xEnd>=yStart){
       container.push(1)
-    }else{
-      console.log("delvis utanför")
-    }
-  }else if(firstSS>=firstFS&&firstSS<=secondFS){
-    console.log("andra är innanför")
-    if(secondSS>=firstFS&&secondSS<=secondFS){
-      console.log("båda är innanför",firstFS,secondFS,",",firstSS,secondSS)
-      container.push(1)
-    }else{
-      console.log("delvis utanför")
-    }
+  }else if(yStart<=xEnd && yStart>=xStart && yEnd<=xEnd && yEnd>=xStart){
+        container.push(1)
   }else{
-    console.log("Utanför")
   }
 })
-console.log(container)
 const total = container.reduce((x,y)=>{return x+y},0)
 console.log(total)
 
