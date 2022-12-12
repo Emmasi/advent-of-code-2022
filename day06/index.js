96ae6a93fdad8b62f26f098a42b851b148fd75fe
Emma <emmaolssondev@outlook.com>
2022-12-12 21:31:36 +0100

    add day 6

--- day06/index.js
+++ day06/index.js
@@ -1,24 +1,22 @@
 const fs = require("fs");
 const { default: test } = require("node:test");
 const testData = fs.readFileSync("testinput.txt", "utf8").split("");
-const data = fs.readFileSync("testinput.txt", "utf8");
+const data = fs.readFileSync("input.txt", "utf8").split("");
 
-console.log(testData);
-// for (let i = 0; i < testData.length; i++) {
-//     const first = testData[i]
-//     const secend = testData[i+1]
-//     console.log(first,secend)
-
-//   }
-const list =[]
-function equal(array) {
-    array.forEach((x)=>{
-        for (let i = 0; i < array.length; i++) {
-            if(x=== array[i]){
-                list.push(x)
-            }
-          }
-     })
+function filtreraDubbleter(array) {
+    const newarray = array.filter((element, index) => array.indexOf(element) === index)
+    return newarray
 }
-equal(testData)
-
+function miniArray(item) {
+  for (let i = 0; i < item.length; i++) {
+    const four = item.slice(i, i + 4);
+    console.log(four);
+    const noDubbleLetterArray = filtreraDubbleter(four);
+    console.log(noDubbleLetterArray)
+    if(noDubbleLetterArray.length > 3){
+      console.log(i+4)
+      break;
+    }
+  }
+}
+miniArray(data);
